f552c4ec1b37f8a70adbbba4ee4819316a746631
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-18 19:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function functionName() {
+  alert(arguments.callee.name);
+}
+
+function AccioJob() {
+  alert(arguments.callee.name);
+}
+
+// Test the functions
+functionName(); // This will display an alert with the text "functionName"
+AccioJob(); // This will display an alert with the text "AccioJob"
